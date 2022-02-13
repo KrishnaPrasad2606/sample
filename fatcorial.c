928f7b28a0ca30a6a47fe9ae7da1423c88af75df
KrishnaPrasad2606 <98822676+KrishnaPrasad2606@users.noreply.github.com>
2022-02-13 09:46:51 +0530

    Add files via upload

---
+++ fatcorial.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int
+main ()
+{
+  int n, f = 1, rem, i, temp;
+  scanf ("%d", &n);
+  for (i = n; i >=1; i--)
+    {
+     f=f*i; 
+	
+	}
+     
+	  printf ("%d", f);
+
+return 0;
+}
