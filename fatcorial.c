89961d4a40e8fe52d49eb67016dd48b1209dbbec
KrishnaPrasad2606 <krishniranju@gmail.com>
2022-02-13 09:54:24 +0530

    I changed the printf

--- fatcorial.c
+++ fatcorial.c
@@ -8,10 +8,10 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <stdio.h>
 
-int
-main ()
+int main ()
 {
   int n, f = 1, rem, i, temp;
+  printf("Enter the number");
   scanf ("%d", &n);
   for (i = n; i >=1; i--)
     {
